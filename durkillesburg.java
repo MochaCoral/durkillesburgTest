0815ee5db6464c5cb50ad2afe91f99806782d275
MochaCoral <90062849+MochaCoral@users.noreply.github.com>
2022-02-04 14:58:44 -0800

    hi

--- durkillesburg.java
+++ durkillesburg.java
@@ -16,3 +16,7 @@ public class durkillesburg {
     }
     
 }
+
+//java -cp ".;libs/junit-4.13.2.jar;libs/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore durkillesburgTester
+
+//javac -cp ".;libs\junit-4.13.2.jar;libs\hamcrest-core-1.3.jar" durkillesburgTester.java    
